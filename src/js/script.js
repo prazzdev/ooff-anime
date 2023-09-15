af00e340b24e4880ff2cb8b24d2d04ab732b393d
Agung Prasetyo <prazzid31@gmail.com>
2023-09-15 10:20:41 +0700

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+let year = new Date().getFullYear()
+document.getElementById('date').innerHTML = year
+
+function openMenu() {
+    let menu = document.getElementById('navmenu')
+    menu.style.display = 'flex'
+}
